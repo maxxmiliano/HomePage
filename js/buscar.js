0f08531fce730f7e0c382fd956c4758f27655785
maxmiliano <maxmilianossouza@hotmail.com>
2023-06-11 22:05:06 -0300

    trabalhando com js

--- js/buscar.js
+++ js/buscar.js
@@ -2,14 +2,29 @@ document.querySelector('form').addEventListener('submit', function(event) {
     event.preventDefault(); // Evita o envio do formulário
     var searchTerm = document.getElementById('searchInput').value;
     var imagePath = 'image/' + searchTerm + '.jpg'; // Constrói o caminho da imagem
-
-    // Lógica adicional, como verificação de existência da imagem, manipulação de erros, etc.
-
-    // Exemplo de ação para atualizar uma imagem existente
-    var imageElement = document.getElementById('myImage');
+  
+    var imageElement = document.querySelector('.image-list img'); // Seleciona a primeira imagem da lista
     if (imageElement) {
-        imageElement.src = imagePath;
+      imageElement.src = imagePath;
+      imageElement.alt = searchTerm; // Define o termo de pesquisa como o texto alternativo da imagem
+  
+      // Adiciona um listener de clique para abrir a modal quando a imagem for clicada
+      imageElement.addEventListener('click', function() {
+        var modal = document.getElementById('myModal');
+        var modalImage = document.getElementById('modalImage');
+        var caption = document.getElementById('caption');
+  
+        modal.style.display = 'block'; // Exibe a modal
+        modalImage.src = this.src; // Define a imagem da modal
+        caption.innerHTML = this.alt; // Define o texto de legenda da modal
+      });
     }
+  });
+  
+  // Adiciona um listener de clique para fechar a modal
+  document.getElementById('closeModal').addEventListener('click', function() {
+    var modal = document.getElementById('myModal');
+    modal.style.display = 'none'; // Esconde a modal
+  });
+  
 
-    // Lógica adicional após a atualização da imagem, se necessário
-});
