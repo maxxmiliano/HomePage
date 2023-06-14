0c51ddba94cf6032ce584d84f8a1b381c4cb9e5b
maxmiliano <maxmilianossouza@hotmail.com>
2023-06-13 22:08:35 -0300

    trabalhando no projeto

--- js/buscar.js
+++ js/buscar.js
@@ -1,30 +1,20 @@
 document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Evita o envio do formulário
-    var searchTerm = document.getElementById('searchInput').value;
-    var imagePath = 'image/' + searchTerm + '.jpg'; // Constrói o caminho da imagem
-  
-    var imageElement = document.querySelector('.image-list img'); // Seleciona a primeira imagem da lista
-    if (imageElement) {
-      imageElement.src = imagePath;
-      imageElement.alt = searchTerm; // Define o termo de pesquisa como o texto alternativo da imagem
-  
-      // Adiciona um listener de clique para abrir a modal quando a imagem for clicada
-      imageElement.addEventListener('click', function() {
-        var modal = document.getElementById('myModal');
-        var modalImage = document.getElementById('modalImage');
-        var caption = document.getElementById('caption');
-  
-        modal.style.display = 'block'; // Exibe a modal
-        modalImage.src = this.src; // Define a imagem da modal
-        caption.innerHTML = this.alt; // Define o texto de legenda da modal
-      });
-    }
+  event.preventDefault(); // Evita o envio do formulário
+  var searchTerm = document.getElementById('searchInput').value;
+  var imagePath = 'image/ford/' + searchTerm + '.jpg'; // Constrói o caminho da imagem
+
+  var enlargedImage = document.getElementById('enlargedImage'); // Seleciona a div para a imagem ampliada
+  var enlargedImageElement = enlargedImage.querySelector('img');
+
+  enlargedImageElement.src = imagePath; // Define a imagem ampliada
+  enlargedImage.style.display = 'block'; // Exibe a div da imagem ampliada
+
+  // Adicione o código para fechar a imagem quando o botão de fechar for clicado
+  const closeButton = document.getElementById('closeButton');
+  closeButton.addEventListener('click', function() {
+    enlargedImage.style.display = 'none';
   });
-  
-  // Adiciona um listener de clique para fechar a modal
-  document.getElementById('closeModal').addEventListener('click', function() {
-    var modal = document.getElementById('myModal');
-    modal.style.display = 'none'; // Esconde a modal
-  });
-  
+});
+
+
 
